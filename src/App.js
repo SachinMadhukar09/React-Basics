ee637c43cf3897dd99131d9b6802c944eae306f0
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-09-26 11:32:20 +0530

    Added React Routing

--- src/App.js
+++ src/App.js
@@ -7,21 +7,40 @@ import Array from "./components/Array";
 import Whizlabs from "./components/Whizlabs";
 import Basics from "./components/Basics";
 import Todo from "./components/Todo";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Movielist from "./components/Movielist";
 import Movieitem from "./components/Movieitem";
 import Forms from "./components/Forms";
 import Authentication from "./components/Authentication";
 import NewTodo from "./components/NewTodo";
+// import ReactRouter from "./components/ReactRouter";
+import Navbar from "./components/Routing/Navbar";
+import Home from "./components/Routing/Home";
+import ContactUs from "./components/Routing/ContactUs";
+import Services from "./components/Routing/Services"
 
-// let age = 13
-// let value =
-
+import {BrowserRouter , Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NewTodo/>
+    {/* React Routing */}
+
+    <BrowserRouter>
+    <Navbar/>
+    <Route path='/home' component={Home} exact />
+
+    <Route path='/contactus' component={ContactUs} exact />
+    <Route path='/services' component={Services} exact />
+
+    </BrowserRouter>
+
+
+
+
+
+
+      {/* <NewTodo/> */}
       {/* <Authentication/> */}
      {/* <Forms/> */}
       {/* <Navbar/>  */}
