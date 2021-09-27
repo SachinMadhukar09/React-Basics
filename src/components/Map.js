1f6ed1638232c282ab9a6ab0dd78d9045b21cde5
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-09-27 19:36:47 +0530

    Added Some More concept of  React Map Function

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,24 +1,62 @@
-import React ,{useState} from 'react'
+import React, { useState } from "react";
+import Authentication from "./Authentication"
+import Form from "./Forms"
 
 function Map() {
+  const [number, setNumber] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [names, setNames] = useState(["Sachin", "Raj", "Bunny", "Kabir"]);
 
-    const [number,setNumber] =useState([1,2,3,4,5,6,7,8,9,10])
-    const [names,setNames] = useState(["Sachin","Raj","Bunny","Kabir"])
+  const [player, setPlayer] = useState([
+    { name: "Sachin", team: "MI" },
+    { name: "Dhoni", team: "CSK" },
+    { name: "Virat", team: "RCB" },
+    { name: "Rohit", team: "MI" },
+  ]);
 
-    const numberList = number.map((number) =>{
-        return <h1>{number}</h1>
-    })
+  const numberList = number.map((number) => {
+    return <h1>{number}</h1>;
+  });
 
-    const nameList = names.map((n)=>{
-        return <h1>{n}</h1>
-    })
+  const nameList = names.map((n) => {
+    return <h1>{n}</h1>;
+  });
+
+  const playerList = player.map((player) => {
     return (
-        <div>
-            <h1>React Maps</h1>
-            {numberList}
-            {nameList}
-        </div>
-    )
+      <div>
+        <h1>The Name of player is {player.name}</h1>
+        <h2>He belongs to {player.team}</h2>
+      </div>
+    );
+  });
+  return (
+    <div>
+      {/* <h1>React Maps</h1> */}
+      {/* {numberList}
+      {nameList}
+      {playerList} */}
+      <h2>Conditional Rendering</h2>
+      {
+        (()=>{
+          if(false) {
+            return<div>
+             <h1>If Statement is Excecuted</h1>
+             {nameList}
+             <Authentication/>
+            </div>
+          }
+          else{
+            return <div>
+              
+            <h2>Else Statement is Excecuted</h2>
+            {playerList}
+            <Form/>
+            </div>
+          }
+        })()
+      }
+    </div>
+  );
 }
 
-export default Map
+export default Map;
